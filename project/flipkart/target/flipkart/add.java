9e949c0f99f5dcfcefeea3fc933891ce90467a47
pallavi-devop <pallavi-devop@pallavi.kawade123@gmail.com>
2023-04-02 02:28:09 -0400

    this is project

---
+++ project/flipkart/target/flipkart/add.java
@@ -0,0 +1,26 @@
+import java.utill;
+class classA
+{
+	void fun()
+	{
+	System.out.println("Enter First number");
+	int a=s.NextInt();
+	
+	System.out.println("Enter First number");
+        int b=s.NextInt();
+
+	System.out.println("Enter First number");
+        int c=s.NextInt();
+
+	int avg =(a+b+c)/3;
+	System.out.println("avarage is "+avg);
+	}
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+classA obj=new classA();
+obj.fun();
+
+
+}
+}
